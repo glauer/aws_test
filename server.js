24e23d7a527d504c640b69fe1f533ec5d1e6755e
glauer <glauer@users.noreply.github.com>
2018-09-25 10:41:44 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('RBCWM Client Portal<br>Build date: 2018-09-17'))
+app.get('/', (req, res) => res.send('RBCWM Client Portal<br>Build date: 2018-09-25'))
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
